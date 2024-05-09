5e419be98bae68a8cea5592494e7655e19bc6a83
JHENYFER VICTORIA NEVES DOS SANTOS <55406272888@info.local>
2024-05-09 08:43:11 -0300

    feat: exercicio 01

---
+++ exemplo.js
@@ -0,0 +1,53 @@
+// //apresentar alguma mensagem na tela
+// alert("Hello Word!!!");//
+
+// //apresenta uma mensgame no console
+// console.log("Hello Word!!!");
+
+
+// //prompt() --> recebe uma entrada digitada
+// //apresenta uma mensagem e pega alguma informação do usuário
+// prompt ("Olá, digite o seu nome:");
+
+//pegando o nome apresentando na tela
+// let nome = prompt("Digite o seu nome: ");
+
+// console.log("Bem vindo! " + nome)
+
+//Concatenação --> juntar texto
+//console.log("Seja bem vindo " + nome + "!!!");
+
+//Fazendo conta com JavaScript
+//parseInt () --> Transforma o que está entre () em número inteiro
+// let nota1 = parseInt(prompt("Digite uma nota: "))
+// let nota2 = parseInt(prompt("Digite outra nota: "))
+// let media = (nota1 + nota2) /2
+// console.log(media)
+
+
+
+
+//parseFloat => transforma oq está entre os ( ) em número com virgula / numero fracionado
+// let produto1= parseFloat (prompt("Digite o valor do produto: "))
+// let produto2= parseFloat (prompt("Digite o valor do segundo produto: "))
+// let soma= produto1 + produto2
+// console.log (soma)
+
+
+// let pontos = 100; 
+//let bonus = parseInt( prompt("quantos pontos você deseja somar?"));
+
+//adição --> num1 + num2
+//Subtrção --> num1 + num 2
+//Multiplicação --> num1 * num 2
+//Divisão -->  num1 / num2
+
+//let totalPontos = pontos + bonus;
+//console.log(totalPontos);
+// let notaAluno = parseFloat( prompt("Digite a nota do aluno"));
+// console.log(notaAluno);
+
+
+//camelCase -Javascrpit
+//PascalCase - Java, C#, Classes
+//snake_case - Python
